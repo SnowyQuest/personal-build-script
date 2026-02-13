afe257d99021b3227d7dfc320afebfab8a10ea47
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2026-02-13 13:49:31 +0300

    Update paths for cloning Xiaomi Zorn repositories

--- crave-zorn.sh
+++ crave-zorn.sh
@@ -5,13 +5,13 @@ repo init --no-repo-verify --git-lfs -u https://github.com/Evolution-X/manifest 
 
 rm -rf device/xiaomi/zorn
 rm -rf device/xiaomi/sm8650-common
-rm -rf kernel/xiaomi/sm8650
+rm -rf device/xiaomi/zorn-kernel
 rm -rf vendor/xiaomi/zorn
 rm -rf hardware/xiaomi
 
 git clone https://github.com/zorn-trees/android_device_xiaomi_zorn -b lineage-23.0 device/xiaomi/zorn
 git clone https://github.com/zorn-trees/device_xiaomi_sm8650-common -b lineage-23.1 device/xiaomi/sm8650-common
-git clone https://github.com/zorn-trees/device_xiaomi_zorn-kernel -b A16 kernel/xiaomi/sm8650
+git clone https://github.com/zorn-trees/device_xiaomi_zorn-kernel -b A16 device/xiaomi/zorn-kernel
 git clone https://github.com/zorn-trees/android_vendor_xiaomi_zorn -b lineage-23.0 vendor/xiaomi/zorn
 git clone https://github.com/zorn-trees/hardware_xiaomi -b lineage-22.2-wip hardware/xiaomi
 
