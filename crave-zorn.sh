828c41ee3bea5d8772ade1b400f0a889c9c17c61
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2026-02-16 20:14:04 +0300

    Update crave-zorn.sh

--- crave-zorn.sh
+++ crave-zorn.sh
@@ -10,7 +10,7 @@ rm -rf hardware/xiaomi
 
 git clone https://github.com/zorn-trees/android_device_xiaomi_zorn -b lineage-23.2 device/xiaomi/zorn
 git clone https://github.com/zorn-trees/device_xiaomi_zorn-kernel -b A16 device/xiaomi/zorn-kernel
-git clone https://github.com/zorn-trees/android_vendor_xiaomi_zorn -b lineage-23.2 vendor/xiaomi/zorn
+git clone https://gitlab.com/SnowyQuest/android_vendor_xiaomi_zorn -b lineage-23.2 vendor/xiaomi/zorn
 git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi
 
 source build/envsetup.sh
