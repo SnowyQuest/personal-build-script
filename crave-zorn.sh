ab9a48be10a9dac5037634eeaccf004dc7c2105c
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2026-03-30 20:29:42 +0300

    Update git clone URLs for Xiaomi Zorn device

--- crave-zorn.sh
+++ crave-zorn.sh
@@ -8,9 +8,9 @@ rm -rf device/xiaomi/zorn-kernel
 rm -rf vendor/xiaomi/zorn
 rm -rf hardware/xiaomi
 
-git clone https://github.com/zorn-trees/android_device_xiaomi_zorn -b lineage-23.2 device/xiaomi/zorn
-git clone https://github.com/zorn-trees/device_xiaomi_zorn-kernel -b A16 device/xiaomi/zorn-kernel
-git clone https://gitlab.com/SnowyQuest/android_vendor_xiaomi_zorn -b lineage-23.2 vendor/xiaomi/zorn
+git clone https://github.com/Xiaomi-zorn-devs/device_xiaomi_zorn -b main device/xiaomi/zorn
+git clone https://github.com/Xiaomi-zorn-devs/device_xiaomi_zorn-kernel -b A16 device/xiaomi/zorn-kernel
+git clone https://codeberg.org/Phhgsi/vendor_xiaomi_zorn -b main vendor/xiaomi/zorn
 git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi
 
 source build/envsetup.sh
