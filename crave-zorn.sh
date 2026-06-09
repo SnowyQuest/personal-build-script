6176888d380354e91ac0ec63d20d96e5faf8e8a2
SnowyQuest <65355034+SnowyQuest@users.noreply.github.com>
2026-06-09 08:47:50 +0300

    Comment out unnecessary git clone commands
    Comment out cloning of LunarisDolby and Sony Dolby vendor.

--- crave-zorn.sh
+++ crave-zorn.sh
@@ -14,8 +14,8 @@ git clone https://github.com/ArmSM/device_xiaomi_zorn -b testZorn device/xiaomi/
 git clone https://github.com/Sakura-Devices/device_xiaomi_zorn-kernel -b 16.2 device/xiaomi/zorn-kernel
 git clone https://codeberg.org/ArmSM/vendor_xiaomi_zorn -b 16 vendor/xiaomi/zorn
 git clone https://github.com/ArmSM/android_hardware_xiaomi -b zorn hardware/xiaomi
-git clone https://github.com/avalon-stuffs/android_packages_apps_LunarisDolby -b 16 packages/apps
-git clone https://github.com/avalon-stuffs/proprietary_vendor_sony_dolby -b 16 vendor/sony/dolby
+#git clone https://github.com/avalon-stuffs/android_packages_apps_LunarisDolby -b 16 packages/apps
+#git clone https://github.com/avalon-stuffs/proprietary_vendor_sony_dolby -b 16 vendor/sony/dolby
 
 source build/envsetup.sh
 lunch lineage_zorn-bp4a-userdebug
